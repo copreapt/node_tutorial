a3abee80d818ec552ee8dec8ed9650be1246f2eb
Oprea Catalin <copreapt@gmail.com>
2023-09-03 23:19:42 +0300

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
-const _ = require('lodash');
+const {readFile, writeFile} = require('fs');
+const util = require('util');
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile);
 
-const items = [1,[2,[3,[4]]]]
-const newItems =_.flattenDeep(items);
 
-console.log(newItems);
+const start = async () => {
+    try {
+        const first = await readFilePromise("./content/first.txt", 'utf-8');
+        const second = await readFilePromise("./content/second.txt",'utf-8');
+        await writeFilePromise('./content/result-mind-grenade.txt', `THIS IS AWESOME: ${first} ${second}`)
+        console.log(first);
+        console.log(second);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
+// getText('./content/first.txt')
+// .then(result => console.log(result))
+// .catch(err => console.log(err))
+
+// const getText = (path) => {
+//     return new Promise((resolve,reject) => {
+//         readFile(path, "utf-8", (err, data) => {
+//           if (err) {
+//             reject(err)
+//           } else {
+//             resolve(data)
+//           }
+//         });
+//     })
+// }
